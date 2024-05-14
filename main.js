69be67a046b2a1e09406dfbf331cb63cd9b6528e
Kinga Hndr <ingainmainz@gmail.com>
2024-05-14 12:12:44 +0200

    odwracanie slow

---
+++ main.js
@@ -0,0 +1,8 @@
+function reversing(word) {
+    let slowo = word.split("");
+    let odwrocone = slowo.reverse()
+    let wyswietlane = odwrocone.join("");
+    console.log(wyswietlane);
+}
+
+reversing("Akademia108"); 
